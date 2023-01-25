0e74777801b7010b7d178c4e9b9a78455c781c30
Brian Goff <cpuguy83@gmail.com>
2023-01-25 19:11:34 +0000

    Add additional testing for multiple connections
    Ensure that everything works with multiple active connections.
    Also refactors the tests into some helpers to improve readability.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- vsock_test.go
+++ vsock_test.go
@@ -1,6 +1,7 @@
 package vsock
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
@@ -17,62 +18,106 @@ func TestVsock(t *testing.T) {
 
 func testVsock(t *testing.T, address string) func(t *testing.T) {
 	return func(t *testing.T) {
-		t.Parallel()
-
 		l, err := Listen("vsock", address)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assertNilError(t, err)
 		defer l.Close()
 
-		type acceptRet struct {
-			conn net.Conn
-			err  error
-		}
-		ch := make(chan acceptRet, 1)
-		go func() {
-			conn, err := l.Accept()
-			ch <- acceptRet{conn, err}
-		}()
+		conn1, conn2 := testDial(t, l)
 
-		addr := l.Addr().(*VsockAddr)
+		pingPong(t, conn1, conn2)
+		pingPong(t, conn1, conn2)
 
-		conn, err := DialVsock(addr.CID, addr.Port)
-		if err != nil {
-			t.Log(addr)
-			t.Fatal(err)
-		}
-		defer conn.Close()
+		conn3, conn4 := testDial(t, l)
 
-		ret := <-ch
-		if ret.err != nil {
-			t.Fatal(ret.err)
-		}
-		defer ret.conn.Close()
+		// Check the original connection is still usable.
+		pingPong(t, conn1, conn2)
+		pingPong(t, conn1, conn2)
 
-		if _, err := conn.Write([]byte("ping")); err != nil {
-			t.Fatal(err)
-		}
+		// Check the new connection is usable.
+		pingPong(t, conn3, conn4)
+		pingPong(t, conn3, conn4)
 
-		p := make([]byte, 4)
-		n, err := ret.conn.Read(p)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if string(p[:n]) != "ping" {
-			t.Fatalf("unexpected message: %s", string(p[:n]))
-		}
+		// Check the original connection is still usable.
+		pingPong(t, conn1, conn2)
+		pingPong(t, conn1, conn2)
 
-		if _, err := ret.conn.Write([]byte("pong")); err != nil {
-			t.Fatal(err)
-		}
-
-		n, err = conn.Read(p)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if string(p[:n]) != "pong" {
-			t.Fatalf("unexpected message: %s", string(p[:n]))
-		}
+		// Check the new connection is usable.
+		pingPong(t, conn3, conn4)
+		pingPong(t, conn3, conn4)
 	}
+}
+
+func testDial(t *testing.T, l net.Listener) (net.Conn, net.Conn) {
+	t.Helper()
+
+	type acceptRet struct {
+		conn net.Conn
+		err  error
+	}
+	ch := make(chan acceptRet, 1)
+	go func() {
+		conn, err := l.Accept()
+		ch <- acceptRet{conn, err}
+	}()
+
+	addr := l.Addr().(*VsockAddr)
+	conn, err := DialVsock(addr.CID, addr.Port)
+	assertNilError(t, err)
+	t.Cleanup(func() {
+		conn.Close()
+	})
+
+	ret := <-ch
+	if ret.err != nil {
+		t.Fatal(ret.err)
+	}
+	t.Cleanup(func() {
+		ret.conn.Close()
+	})
+	return conn, ret.conn
+}
+
+func assertNilError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertLen[T any](t *testing.T, v []T, n int) {
+	t.Helper()
+	if len(v) != n {
+		t.Fatalf("exepcted length %d, got: %d", n, len(v))
+	}
+}
+
+func assertEq(t *testing.T, a, b []byte) {
+	t.Helper()
+	if !bytes.Equal(a, b) {
+		t.Fatalf("expected %v, got: %v", a, b)
+	}
+}
+
+func pingPong(t *testing.T, conn1, conn2 net.Conn) {
+	ping := []byte("ping")
+	pong := []byte("pong")
+
+	n, err := conn1.Write(ping)
+	assertNilError(t, err)
+	assertLen(t, ping, n)
+
+	buf := make([]byte, 4)
+	n, err = conn2.Read(buf)
+	assertNilError(t, err)
+	assertLen(t, buf, n)
+	assertEq(t, ping, buf)
+
+	n, err = conn2.Write(pong)
+	assertNilError(t, err)
+	assertLen(t, pong, n)
+
+	n, err = conn1.Read(buf)
+	assertNilError(t, err)
+	assertLen(t, buf, n)
+	assertEq(t, pong, buf)
 }
